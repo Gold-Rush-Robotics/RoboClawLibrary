e3c701b604e09747a93238d51af5a7766875d535
Philip Smith <philip@randomsmiths.com>
2024-01-08 19:35:26 -0500

    create files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RoboClawLibrary
+
+This is mostly based on [basic micros python library for roboclaws](https://github.com/basicmicro/roboclaw_python_library)
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='grr_roboclaw',
+    packages=find_packages(),
+    version='0.1.0',
+    description='grr roboclaw',
+    author='Philip Smith',
+)
